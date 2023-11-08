aedb583d27e85d3714e6f67e50ad06554bec9c06
Otavio Cunha <otaviocunhaps1@gmail.com>
2023-11-08 02:23:49 -0300

    quiz 1 no ar

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,18 +1,86 @@
 import HeaderComponent from "../../components/header/Header";
-import React from "react";
-
+import React, { useState } from "react";
 
 import "./Projects.scss";
 
 function Projects() {
- 
+  const questions = [
+    {
+      question: "Qual a cor favorita do Otávio?",
+      options: [ "Verde", "Vermelho", "Azul", "Amarelo"],
+      answer: "Azul",
+    },
+    {
+      question: "A primeira cartinha que o Otavio fez foi:",
+      options: [
+        "Do Senhor dos Anéis",
+        "De Star Wars",
+        "Do Harry Potter",
+        "De Pokémon",
+      ],
+      answer: "Do Harry Potter",
+    },
+    { 
+      question: "Qual o time do Otávio?",
+      options: ["Grêmio", "Inter", "Santos", "Corinthians"],
+      answer: "Grêmio",
+    },
+    {
+      question: "Qual é o tipo de filme favorito do Otávio?",
+      options: ["Comédia", "Ação", "Romance", "Terror"],
+      answer: "Terror",
+    },
+    {
+      question: "Qual série o Otávio perdeu a aposta dos beijos?",
+      options: [
+        "Boba Fett",
+        "Breaking Bad",
+        "Stranger Things",
+        "Game of Thrones",
+      ],
+      answer: "Boba Fett",
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [quizCompleted, setQuizCompleted] = useState(false);
+
+  const handleAnswer = (selectedOption) => {
+    if (selectedOption === questions[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setQuizCompleted(true);
+    }
+  };
 
   return (
-   
     <div className="app">
       <HeaderComponent />
-      <h1>Projects</h1>
-
+      <div className="content">
+        {quizCompleted ? (
+          <div>
+            <h1>Quiz1 completo!</h1>
+            <p className="questions">
+              Parabéns você acertou: {score} de {questions.length}
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h1>Conhece mesmo o Otávio?</h1>
+            <p className="questions">{questions[currentQuestion].question}</p>
+              {questions[currentQuestion].options.map((option, index) => (
+                <button className="button" onClick={() => handleAnswer(option)}>
+                  {option}
+                </button>
+              ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
