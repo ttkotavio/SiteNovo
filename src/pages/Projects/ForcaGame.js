cd167d45b1cfee677f36e030f686f30a12180af4
Otavio Cunha <otaviocunhaps1@gmail.com>
2023-11-08 21:08:31 -0300

    fix

--- src/pages/Projects/ForcaGame.js
+++ src/pages/Projects/ForcaGame.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import "./ForcaGames.scss";
+import React, { useState, useLayoutEffect } from 'react';
+import Confetti from 'react-confetti';
+import './ForcaGames.scss';
 
 function ForcaGame() {
-  const secretPhrase = "quer namorar comigo?";
-  const words = secretPhrase.split(" "); // Divide a frase em palavras
-  const initialDisplayPhrase = words.map(word => word.replace(/./g, "_")); // Inicializa a frase oculta
+  const secretPhrase = 'quer namorar comigo?';
+  const words = secretPhrase.split(' '); // Divide a frase em palavras
+  const initialDisplayPhrase = words.map((word) => word.replace(/./g, '_')); // Inicializa a frase oculta
 
   const [displayPhrase, setDisplayPhrase] = useState(initialDisplayPhrase);
 
   const [usedLetters, setUsedLetters] = useState([]);
-  const maxAttempts = 10;
-  const [attemptsLeft, setAttemptsLeft] = useState(maxAttempts);
+  const [attemptsLeft, setAttemptsLeft] = useState(5); // Número inicial de tentativas
+  const [showConfetti, setShowConfetti] = useState(false);
 
   const handleGuess = (guess) => {
     if (usedLetters.includes(guess)) {
@@ -38,29 +39,74 @@ function ForcaGame() {
 
     setDisplayPhrase(phraseUpdated);
 
-    if (!secretPhrase.includes(guess)) {
+    if (!secretPhrase.includes(guess)) { 
       setAttemptsLeft(attemptsLeft - 1);
+    }
+
+    // Verifica se todas as letras foram adivinhadas
+    const isPhraseComplete = displayPhrase.every((word) => !word.includes('_'));
+    if (isPhraseComplete) {
+      setAttemptsLeft(0); // Define o número de tentativas para 0
+      setShowConfetti(true); // Mostra o confete
     }
   };
 
   const renderAlphabetButtons = () => {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz?-!#$%*";
-    return alphabet.split("").map((letter) => (
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz?-!#$%*';
+    return alphabet.split('').map((letter) => (
       <button key={letter} onClick={() => handleGuess(letter)}>
         {letter.toUpperCase()}
       </button>
     ));
   };
 
+  const handleNo = () => {
+    // Implemente o código para mover o botão "NÃO" para baixo
+    // Isso pode ser feito usando CSS ou animações JavaScript.
+    // Por exemplo, você pode adicionar uma classe CSS ao botão "NÃO"
+    // que faz ele se mover para baixo.
+  };
+
+  const handleYes = () => {
+    // Outras ações que você deseja realizar ao clicar em "SIM"
+  };
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      // Atualiza o tamanho da janela aqui, se necessário
+    }
+
+    window.addEventListener('resize', updateSize);
+    updateSize();
+
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
   return (
     <div className="forca-game">
       <h1>Jogo da Forca</h1>
       <p className="phrase-label">Frase a ser adivinhada:</p>
       {displayPhrase.map((word, index) => (
-        <p key={index} className="phrase">{word}</p>
+        <p key={index} className="phrase">
+          {word}
+        </p>
       ))}
       <p className="attempts-left">Tentativas restantes: {attemptsLeft}</p>
       <div className="alphabet-buttons">{renderAlphabetButtons()}</div>
+
+      {attemptsLeft === 0 && (
+        <div className="end-game-buttons">
+          <button onClick={handleNo}>NÃO</button>
+          <button onClick={handleYes}>SIM</button>
+        </div>
+      )}
+
+      {showConfetti && (
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+        />
+      )}
     </div>
   );
 }
